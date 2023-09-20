9f2db21f112c30f6d7f2ff94c43dccac2ae36c99
gbd850 <mmyszaki@gmail.com>
2023-09-20 22:50:09 +0200

    Added metadata to response

--- src/main/java/dev/peter/dataaggregator/dto/DataResponse.java
+++ src/main/java/dev/peter/dataaggregator/dto/DataResponse.java
@@ -1,5 +1,6 @@
 package dev.peter.dataaggregator.dto;
 
+import dev.peter.dataaggregator.model.MetaData;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,6 @@ public class DataResponse {
     private int id;
     private String breed;
     private String origin;
+    private MetaData meta;
 
 }
---
+++ src/main/java/dev/peter/dataaggregator/model/MetaData.java
@@ -0,0 +1,14 @@
+package dev.peter.dataaggregator.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MetaData {
+    private String coat;
+//    private String other_names;
+    private String breed_status;
+}
