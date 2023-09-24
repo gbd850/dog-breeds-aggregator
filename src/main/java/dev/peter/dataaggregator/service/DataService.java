c4796985b72498042538da13d16768ec78e11a66
gbd850 <mmyszaki@gmail.com>
2023-09-24 22:32:33 +0200

    Hide API key

--- src/main/java/dev/peter/dataaggregator/service/DataService.java
+++ src/main/java/dev/peter/dataaggregator/service/DataService.java
@@ -3,6 +3,7 @@ package dev.peter.dataaggregator.service;
 import dev.peter.dataaggregator.dto.DataFormat;
 import dev.peter.dataaggregator.dto.DataResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,10 +17,13 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 public class DataService {
+
+    @Value("${apiKey}")
+    private String apiKey;
     public List<DataFormat> fetchData(List<String> filters) {
         HttpHeaders headers = new HttpHeaders();
         RestTemplate restTemplate = new RestTemplate();
-        headers.add("X-RapidAPI-Key", "49ff0941bbmsha963dcd0964c181p10a44ajsn508b93f28735");
+        headers.add("X-RapidAPI-Key", apiKey);
         headers.add("X-RapidAPI-Host", "dog-breeds2.p.rapidapi.com");
         headers.setContentType(MediaType.APPLICATION_XML);
         HttpEntity<String> request = new HttpEntity<>(headers);
